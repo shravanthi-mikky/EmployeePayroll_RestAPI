3c000e00e76d589822bb64295ab7d3e29d383ce0
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-06-21 13:09:40 +0530

    [shravanthi] - retrieving Employee Data from Json file using Rest API

---
+++ Employee_RestAPI/RestSharpTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_RestAPI
+{
+    public class RestSharpTest
+    {
+        public class Employee
+        {
+            public int id { get; set; }
+            public string Name { get; set; }
+            public string Salary { get; set; }
+        }
+    }
+}
